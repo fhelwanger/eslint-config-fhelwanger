23107afe657917914e8b6300d427680d2bd6cd62
fhelwanger <fhelwanger@gmail.com>
2016-01-21 22:42:57 -0200

    Split files by environment

---
+++ es6.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "extends": "fhelwanger/base",
+
+  "ecmaFeatures": {
+    "modules": true
+  },
+
+  "env": {
+    "es6": true
+  },
+
+  "parser": "babel-eslint",
+
+  "rules": {
+    "arrow-parens": [ 2, "as-needed" ],
+    "arrow-spacing": 2,
+    "constructor-super": 2,
+    "generator-star-spacing": [ 2, {
+      "before": false,
+      "after": true
+    }],
+    "no-arrow-condition": 2,
+    "no-class-assign": 2,
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-this-before-super": 2,
+    "no-var": 2,
+    "object-shorthand": 2,
+    "prefer-const": 2,
+    "prefer-spread": 2,
+    "prefer-template": 2,
+    "require-yield": 2
+  }
+};
